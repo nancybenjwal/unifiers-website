c78c81bb18e95f380004f296e9fe8bf3fba3d933
nancybenjwal <benjwalnancy@gmail.com>
2023-06-17 11:39:11 +0530

    #

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/404.js")),
+  "component---src-pages-consulting-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/consulting.js")),
+  "component---src-pages-education-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/education.js")),
+  "component---src-pages-hero-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/hero.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/index.js")),
+  "component---src-pages-skilling-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/skilling.js")),
+  "component---src-pages-team-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/pages/team.js")),
+  "component---src-templates-using-dsg-js": preferDefault(require("/home/unifiers/unifiers/unifiers-website/src/templates/using-dsg.js"))
+}
+
---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
+    },{
+      plugin: require('../node_modules/gatsby-omni-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"enableListener":true,"preconnect":["https://fonts.googleapis.com","https://fonts.gstatic.com"],"web":[{"name":"Poppins","file":"https://fonts.googleapis.com/css2?family=Inter:wght@100&family=Poppins:wght@600&display=swap"}]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
